03c2ddbe49f333396807706e120aafd880dceeb7
Barrakuda <gterov@gmail.com>
2020-08-04 20:46:39 +0300

    Added magazine slider to repository

---
+++ script.js
@@ -0,0 +1,23 @@
+const bigImage = document.getElementById("big-image");
+
+function nextImg(event) {
+  event = event || window.event;
+
+  if (event.target.classList.contains("min-image")) {
+    const allMinImg = document.querySelectorAll(".min-img-block div");
+
+    for (let i = 0; i < allMinImg.length; i++) {
+      allMinImg[i].classList.remove("active");
+    }
+
+    bigImage.src = event.target.src;
+    event.target.parentNode.classList.add("active");
+  }
+}
+
+document.body.addEventListener("mousedown", nextImg);
+
+function resizeImage() {
+  console.log(bigImage);
+}
+bigImage.addEventListener("mousedown", resizeImage);
